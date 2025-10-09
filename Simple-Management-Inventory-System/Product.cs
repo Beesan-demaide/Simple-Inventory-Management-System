ae5fe210e5b072620ee8d1d77f5f3e3f6fa73f8c
Beesan-demaide <beesan.demaide@gmail.com>
2025-10-09 15:47:10 +0300

    Add product class properties

--- Simple-Management-Inventory-System/Product.cs
+++ Simple-Management-Inventory-System/Product.cs
@@ -8,5 +8,31 @@ namespace Simple_Management_Inventory_System
 {
     public class Product
     {
+        private string name;
+        private int quantity;
+        private int price;
+
+        public Product(string name, int quantity, int price)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value.Length > 50 ? value[..50] : value; }
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
     }
 }
